1411f6e3927f589b9fcd5cd821d1092a580b066e
Abirami <abiramisd16@gmail.com>
2025-12-22 18:15:43 +0530

    feat: Implement tabbed navigation for Users and Chat History in HomePage

--- lib/features/home/views/home_page.dart
+++ lib/features/home/views/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mysivi_chat_app/features/home/tabs/chat_history/views/chat_history_tab.dart';
+import 'package:flutter_mysivi_chat_app/features/home/tabs/users/views/users_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView();
+    return DefaultTabController(
+      length: 2,
+      child: NestedScrollView(
+        headerSliverBuilder: (widgets, context) => [
+          const SliverAppBar(
+            pinned: true,
+            floating: true,
+            snap: false,
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'Users'),
+                Tab(text: 'Chat History'),
+              ],
+            ),
+          ),
+        ],
+        body: const TabBarView(
+          physics: ScrollPhysics(),
+          children: [UsersTab(), ChatHistoryTab()],
+        ),
+      ),
+    );
   }
 }
